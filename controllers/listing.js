cf75d3e6bee6fe07b629b04810c60a4b72cf7988
Umang Khemka <umangkhemka2005@gmail.com>
2025-04-23 23:42:27 +0530

    adding controllers

---
+++ controllers/listing.js
@@ -0,0 +1,51 @@
+const Listing = require("../models/listing");
+
+
+module.exports.index = async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+};
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("listings/new.ejs");
+};
+
+module.exports.showListing = async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id).populate({path: "reviews",populate: {path: "author",},}).populate("owner");
+  if (!listing) {
+    req.flash("error","Listing you requested does not exist");
+     return res.redirect("/listings");
+  }
+  res.render("listings/show.ejs", { listing });
+};
+
+module.exports.createListing = async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  await newListing.save();
+  req.flash("success", "New listing created!");
+  res.redirect("/listings");
+};
+
+module.exports.editListing = async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) throw new ExpressError(404, "Listing not found!");
+  res.render("listings/edit.ejs", { listing });
+};
+
+module.exports.updateListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", "Listing Updated !");
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.deleteListing = async (req, res) => {
+  const { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  req.flash("success", "Listing deleted!");
+  if (!deletedListing) throw new ExpressError(404, "Listing not found!");
+  res.redirect("/listings");
+};
