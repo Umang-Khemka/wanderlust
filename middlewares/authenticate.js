1a68357df50758dd99042d7a99327fd2adf430a9
Umang Khemka <umangkhemka2005@gmail.com>
2025-04-23 23:40:20 +0530

    added middleware for author and owner

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,7 +1,11 @@
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
+
 module.exports.isLoggedIn = (req,res,next) => {
     if(!req.isAuthenticated()){
         req.session.redirectUrl = req.originalUrl;
-        req.flash("error", "You must be logged in to create a listing.");
+        req.flash("error", "You must be logged in first!!");
         return res.redirect("/login");
     }
     next();
@@ -10,6 +14,26 @@ module.exports.isLoggedIn = (req,res,next) => {
 module.exports.saveRedirectUrl = (req,res,next)=> {
     if(req.session.redirectUrl){
         res.locals.redirectUrl = req.session.redirectUrl
+    }
+    next();
+};
+
+module.exports.isOwner = async (req,res,next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)){
+      req.flash("error", "You are not the owner of this listing!"); 
+      return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
+module.exports.isReviewAuthor = async (req,res,next) => {
+    let { id,reviewId } = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+      req.flash("error", "You are not the author of this review"); 
+      return res.redirect(`/listings/${id   }`);
     }
     next();
 };
