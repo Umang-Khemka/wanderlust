988eaf2aea89aeb1f754aebf19561a13907d853d
Umang Khemka <umangkhemka2005@gmail.com>
2025-04-23 23:41:55 +0530

    added owner of listing

--- init/index.js
+++ init/index.js
@@ -19,6 +19,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj)=> ({...obj,owner: "6807cccfd9e1c4ebb39f5c22"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
