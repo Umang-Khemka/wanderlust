9de2facd077a539d8cd7c50c9603874e7233720e
Umang Khemka <umangkhemka2005@gmail.com>
2025-04-26 11:00:37 +0530

    optimized

--- routes/review.js
+++ routes/review.js
@@ -1,39 +1,14 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const Review = require("../models/review.js");
-const Listing = require("../models/listing.js");
 const { validateReview } = require("../middlewares/validateReview.js");
 const { isLoggedIn, isReviewAuthor } = require("../middlewares/authenticate.js");
+const reviewController = require("../controllers/reviews.js");
 
 // Post route for creating a review
-router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-  newReview.author = req.user._id;
-  listing.reviews.push(newReview);
-  await newReview.save();
-  await listing.save();
-
-  req.flash("success", "New review created!");
-  res.redirect(`/listings/${listing._id}`);
-}));
+router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
 
 // Delete route for deleting a review
-router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
-  const { id, reviewId } = req.params;
-
-  const listing = await Listing.findById(id);
-  if (!listing) {
-    req.flash("error", "Listing not found!");
-    return res.redirect("/listings");
-  }
-
-  await Review.findByIdAndDelete(reviewId);
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-
-  req.flash("success", "Review deleted!");
-  res.redirect(`/listings/${id}`);
-}));
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.deleteReview));
 
 module.exports = router;
