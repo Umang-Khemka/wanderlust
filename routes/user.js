0aeef64c7471415fea454de6214a52ee2264bee5
Umang Khemka <umangkhemka2005@gmail.com>
2025-04-26 11:01:08 +0530

    optimized

--- routes/user.js
+++ routes/user.js
@@ -1,59 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-const {saveRedirectUrl} = require("../middlewares/authenticate.js")
+const { saveRedirectUrl } = require("../middlewares/authenticate.js");
+const userController = require("../controllers/users.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
+// Signup Routes
+router.route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
 
-router.post("/signup",wrapAsync(async(req,res) => {
-    try {
-        let {username,email,password} = req.body;
-        const newUser = new User({email,username});
-        const registeredUser = await User.register(newUser,password)
-        await req.login(registeredUser,(err)=> {
-          if(err){
-            return next(err);
-          }
-          req.flash("success", "Welcome to WanderLust!");
-          res.redirect("/listings");    
-        });
-    } catch(e) {
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
-})); 
+// Login Routes
+router.route("/login")
+  .get(userController.loginRenderForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true
+    }),
+    userController.login
+  );
 
-router.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
+// Logout Route
+router.get("/logout", userController.logout);
 
-router.post(
-  "/login",
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true
-  }),
-  async (req, res) => {
-    const redirectUrl = res.locals.redirectUrl || "/listings";
-    delete req.session.redirectUrl; 
-    req.flash("success", "Welcome back!");
-    res.redirect(redirectUrl);
-  }
-);
-
-
-router.get("/logout",(req,res,next)=> {
-  req.logOut((err)=> {
-    if(err){
-      return next(err);
-    }
-    req.flash("success", "Successfully logged out!");
-    return res.redirect("/login");
-  });
-});
 module.exports = router;
