537db76b897c56d4ca8c26715eda82b0f8dd6d04
Umang Khemka <umangkhemka2005@gmail.com>
2025-04-26 10:59:46 +0530

    added multer functionality

--- routes/listing.js
+++ routes/listing.js
@@ -3,33 +3,29 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const validateId = require("../middlewares/validateId");
-const {isLoggedIn,isOwner} = require("../middlewares/authenticate.js");
-const {validateListing} = require("../middlewares/validateListing");
-
+const { isLoggedIn, isOwner } = require("../middlewares/authenticate.js");
+const { validateListing } = require("../middlewares/validateListing");
 const listingController = require("../controllers/listing.js");
-
+const multer  = require("multer")
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 // Index Route
-router.get("/", wrapAsync(listingController.index));
+router.route("/")
+  .get(wrapAsync(listingController.index))
+  .post(isLoggedIn,  upload.single("listing[image]"),validateListing,wrapAsync(listingController.createListing));
+
 
 // New Route
-router.get("/new", isLoggedIn,listingController.renderNewForm);
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// Show Route
-router.get("/:id", validateId, wrapAsync(listingController.showListing));
+// Show, Edit, Update, Delete Routes
+router.route("/:id")
+  .get(validateId, wrapAsync(listingController.showListing))
+  .put(isLoggedIn, isOwner,upload.single("listing[image]"), validateId, validateListing, wrapAsync(listingController.updateListing))
+  .delete(isLoggedIn, isOwner, validateId, wrapAsync(listingController.deleteListing));
 
-
-// Create Route
-router.post("/", isLoggedIn,validateListing, wrapAsync(listingController.createListing));
-
-
-// Edit Route
-router.get("/:id/edit", isLoggedIn,isOwner,validateId, wrapAsync(listingController.editListing));
-
-// Update Route
-router.put("/:id",isLoggedIn,isOwner, validateId, validateListing, wrapAsync(listingController.updateListing));
-
-// Delete Route
-router.delete("/:id",isLoggedIn,isOwner, validateId, wrapAsync(listingController.deleteListing));
+// Edit Route (Separate route for clarity, could be combined with Show and Update routes)
+router.get("/:id/edit", isLoggedIn, isOwner, validateId, wrapAsync(listingController.editListing));
 
 module.exports = router;
