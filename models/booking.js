0551880a978d7f62c718876e8a94c924cef157e4
Umang Khemka <umangkhemka2005@gmail.com>
2025-05-31 15:41:08 +0530

    new model

---
+++ models/booking.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const bookingSchema = new Schema({
+  listing: {
+    type: Schema.Types.ObjectId,
+    ref: "Listing",
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: true
+  }
+});
+
+
+const Booking = mongoose.model("Booking", bookingSchema);
+module.exports = Booking;
